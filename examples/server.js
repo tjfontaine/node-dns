ccbc6407033926016520d8e5474f3fc35f68e7e8
Timothy J Fontaine <tjfontaine@gmail.com>
2011-09-13 14:07:36 -0400

    everything is a response, response handles unpack

--- examples/server.js
+++ examples/server.js
@@ -4,8 +4,9 @@ var server = dns.createServer('udp4')
 server.bind(5353)
 
 server.on('request', function(request, response) {
+  console.log(request)
   response.answer.push(new dns.types.A({
-    name: request.questions[0].name,
+    name: request.question[0].name,
     address: '127.0.0.1',
     ttl: 600,
   }))
