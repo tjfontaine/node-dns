a14e4d890f6581101be32ac9e5db9321beac0c8e
Timothy J Fontaine <tjfontaine@gmail.com>
2012-02-29 11:17:56 -0500

    add packet round trip test

---
+++ test/packet.js
@@ -0,0 +1,32 @@
+var dns = require('../dns'),
+  Packet = require('../lib/packet');
+
+exports.roundTrip = function (test) {
+  var buff, pre, post;
+
+  pre = new Packet();
+  pre.header.id = 12345;
+  pre.header.rcode = 1;
+
+  pre.question.push(dns.Question({
+    name: 'www.google.com',
+    type: dns.consts.NAME_TO_QTYPE.A,
+  }));
+
+  pre.answer.push(dns.A({
+    name: 'www.google.com',
+    address: '127.0.0.1',
+    ttl: 600,
+  }));
+
+  buff = new Buffer(pre.estimateSize());
+
+  pre.pack(buff, 0);
+
+  post = new Packet();
+  post.unpack(buff, true);
+
+  test.ok(pre.compare(post));
+
+  test.done();
+};
