9171d2624e165af2d6141f79c9e767173ffd7aa3
Timothy J Fontaine <tjfontaine@gmail.com>
2012-07-08 12:38:42 -0400

    if not enough space in buffer truncate packet

--- test/packet.js
+++ test/packet.js
@@ -30,3 +30,43 @@ exports.roundTrip = function (test) {
   test.deepEqual(pre, post);
   test.done();
 };
+
+exports.truncate = function (test) {
+  var buff, pre, post, i;
+
+  pre = new Packet();
+  pre.header.id = 12345;
+  pre.header.rcode = 1;
+
+  pre.question.push(dns.Question({
+    name: 'really.long.name.some.domain.com',
+    type: 'A',
+  }));
+
+  for (i = 0; i < 254; i ++) {
+    pre.answer.push(dns.A({
+      name: i+'.'+i+'.'+i+'.really.long.name.some.domain.com',
+      address: '127.0.0.' + i,
+      ttl: 600,
+    }));
+    pre.authority.push(dns.A({
+      name: i+'.'+i+'.'+i+'.really.long.name.some.domain.com',
+      address: '127.0.0.' + i,
+      ttl: 600,
+    }));
+    pre.additional.push(dns.A({
+      name: i+'.'+i+'.'+i+'.really.long.name.some.domain.com',
+      address: '127.0.0.' + i,
+      ttl: 600,
+    }));
+  }
+
+  buff = new Buffer(512);
+  len = writer(buff, pre);
+  post = parser(buff.slice(0, len));
+
+  test.notEqual(pre.additional.length, post.additional.length,
+    'Additional should be less because of truncated packet');
+
+  test.done();
+};
